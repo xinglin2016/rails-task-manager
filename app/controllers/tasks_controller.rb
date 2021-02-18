a4eba27fbbca33b91fc878c9faa22cc1f3449400
xinglin2016 <lixinglin0829@gmail.com>
2021-02-18 07:32:52 -0500

    add redirect in controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -14,17 +14,19 @@ class TasksController < ApplicationController
   def create
     @task = Task.new(task_params)
     @task.save
-    # redirect_to restaurant_path(@restaurant)
+    redirect_to tasks_path
   end
 
   def edit; end
 
   def update
     @task.update(task_params)
+    redirect_to task_path(@task)
   end
 
   def destroy
     @task.destroy
+    redirect_to tasks_path
   end
 
   private
